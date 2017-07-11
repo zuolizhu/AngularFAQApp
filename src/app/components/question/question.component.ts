de7ebb1ffb0a23555b24bfbaad67a3d8bb4a4555
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-11 17:55:38 -0400

    convert input into JSON and save into localdatabase

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 import { Question } from '../../models/Question';
 
@@ -11,9 +12,13 @@ import { Question } from '../../models/Question';
 export class QuestionComponent implements OnInit {
   @Input('question') question:Question;
 
-  constructor() { }
+  constructor(public dataService: DataService) {
+   }
 
   ngOnInit() {
+  }
+  removeQuestion(question) {
+    this.dataService.removeQuestion(question);
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,31 +6,45 @@ export class DataService {
   questions: Question[];
 
   constructor() {
-    this.questions = [
-      {
-        text: 'What is your favorite car brand ?',
-        answer: 'Mercedes-Benz',
-        hide: true
-      },
-      {
-        text: 'Where do you live ?',
-        answer: 'New York',
-        hide: true
-      },
-      {
-        text: 'Where do you study ?',
-        answer: 'SUNY Albany',
-        hide: true
-      }
-    ];
+    // this.questions = [
+    //   {
+    //     text: 'What is your favorite car brand ?',
+    //     answer: 'Mercedes-Benz',
+    //     hide: true
+    //   },
+    //   {
+    //     text: 'Where do you live ?',
+    //     answer: 'New York',
+    //     hide: true
+    //   },
+    //   {
+    //     text: 'Where do you study ?',
+    //     answer: 'SUNY Albany',
+    //     hide: true
+    //   }
+    // ];
   }
 
   getQuestions() {
+
+    if(localStorage.getItem('questions') == null) {
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+
     return this.questions;
   }
 
   addQuestion(question: Question){
     this.questions.unshift(question);
+  }
+  removeQuestion(question: Question){
+    for(let i = 0; i < this.questions.length; i++) {
+      if(question == this.questions[i]) {
+        this.questions.splice(i, 1);
+      }
+    }
   }
 
 }
