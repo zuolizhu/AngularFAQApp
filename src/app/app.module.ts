06fa3cd7e6cf06c61a7c1b413d9a88f7c24417ac
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-11 16:09:41 -0400

    component set up
    interesting...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
