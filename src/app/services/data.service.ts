a101267722d11ba5295c987e709b5e9ec8491edd
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-11 18:04:15 -0400

    Add and remove function added

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -25,6 +25,7 @@ export class DataService {
     // ];
   }
 
+//Get Questions from Local Storage
   getQuestions() {
 
     if(localStorage.getItem('questions') == null) {
@@ -36,13 +37,36 @@ export class DataService {
     return this.questions;
   }
 
+//Add Questions into Local Storage
   addQuestion(question: Question){
     this.questions.unshift(question);
+
+    //Initialize a local var
+    let questions;
+
+    if(localStorage.getItem('questions') == null) {
+      questions = [];
+      //Push new question
+      questions.unshift(question);
+      //Set new array to Local Storage
+      //stringify convert JSON into string
+      localStorage.setItem('questions', JSON.stringify(questions));
+
+    } else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+      //Add new question
+      questions.unshift(question);
+      //Reset local storage
+      localStorage.setItem('questions',JSON.stringify(questions));
+    }
   }
+
+  //Remove Questions from Local Storage
   removeQuestion(question: Question){
     for(let i = 0; i < this.questions.length; i++) {
       if(question == this.questions[i]) {
         this.questions.splice(i, 1);
+        localStorage.setItem('questions',JSON.stringify(this.questions));
       }
     }
   }
